2300786c07f37c63e409fe42cb3b2b5a2f7c986d
amalsabu59 <amalsabu59@gmail.com>
2023-11-10 18:33:49 +0530

    changes related to deployment

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://tic-tac-toe-client-tau.vercel.app",
     methods: ["GET", "POST"],
   },
 });
